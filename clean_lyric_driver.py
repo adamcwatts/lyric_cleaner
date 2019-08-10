9dd9428b6d38960e2b42ff5787a4930bd070d806
adamcwatts <aldburg@gmail.com>
2019-08-10 17:01:01 -0400

    added method for dealing with lyrics as .text file Example original lyric and lyric cleaned up

--- clean_lyric_driver.py
+++ clean_lyric_driver.py
@@ -1,1 +1,32 @@
-print('hello world')
+# TODO: 1. access file directory besides mine (works for other users) 2.  open each file and parse for bad lyrics. 4. overwrite file
+
+import os
+import csv
+import txt_lyrics
+
+
+def file_ending_parser(file_list):
+    file_endings = ['.txt', '.lrc']
+    text_lyrics = []
+    lrc_lyrics = []
+    unknown_lyrics = []
+
+    for file in file_list:
+        if file_endings[0] in file:
+            text_lyrics.append(file)
+        elif file_endings[1] in file:
+            lrc_lyrics.append(file)
+        else:
+            unknown_lyrics.append(file)
+
+    return text_lyrics, lrc_lyrics
+
+
+if __name__ == '__main__':
+    os.chdir(r'C:\Users\adamc\foobar_lyrics')
+    list_of_files = os.listdir()
+
+    list_of_text_lyrics, list_of_lrc_lyrics = file_ending_parser(list_of_files)
+
+    print('Number of lyric files to parse through:', len(list_of_files))
+    print(list_of_text_lyrics)
---
+++ txt_lyrics.py
@@ -0,0 +1,96 @@
+import csv
+
+
+class LyricsFromText:
+
+    def __init__(self, lyric):
+        def tail_finder():
+            length_of_lyrics = len(lyric)
+            # print(length_of_lyrics)
+            flag = True
+            counter = 10
+
+            if length_of_lyrics > 1:
+                while flag:
+                    try:
+                        tail = lyric[-counter:]
+                        index = length_of_lyrics - counter
+                        return tail, index
+                    except IndexError:
+                        flag = True
+                        counter -= 1
+
+        self.head = (lyric[0])  # retries header (1st line of lyrics file)
+        (self.tail, self.index) = tail_finder()  # retries tail and index (last few lines and the location of end)
+        self.middle = lyric[1:self.index]  # middle chunk of lyrics
+
+        # print(self.tailing)
+
+
+def fix_header():
+    title_position = lyric_obj.head[0].find('Title : ')
+
+    if title_position != -1:
+        new_header = [
+            [lyric_obj.head[0][:title_position]],  # e.g. 'Artist : Black Sabbath'
+            ['Title : ' + song_title],  # e.g. 'Title : Iron Man'
+            [lyric_obj.head[0].split(song_title)[1]],  # e.g. 'Has he lost his Mind?'
+            []  # empty line between header and lyrics
+        ]
+        return new_header
+    else:  # create titles for lyrics if none are already in the txt file
+        new_header = [[f'Artist : {artist}'],
+                      [f'Title : {song_title}'],
+                      []]
+        return new_header
+
+
+def fix_tail():
+    texts_to_remove = [
+        'Credits',  # remove string
+        'Lyrics licensed by',  # Remove all lines below this
+        'External links',  # sometimes line above is not included, thus remove all below this
+        'Nominate as Song of the Day',  # also should be removed
+        'iTunes: ',  # also should be removed
+    ]
+    new_tail = []
+    flag = False
+
+    for line in lyric_obj.tail:  # iterate over all the nested list
+        for text in texts_to_remove:  # iterate over list
+            if text in line[0]:
+                if text == texts_to_remove[0]:  # 'Credits' is found
+                    new_string = line[0].split(texts_to_remove[0])[0]  # remove the 'Credits' from lyrics
+                    new_tail.append([new_string])
+                    break  # return to outer loop
+
+                else:  # found bad text
+                    return new_tail  # failed condition and rest of
+
+            elif text == texts_to_remove[-1]:  # faster to check condition or use enumerate and check number?
+                # lyrics are legit and only append after iterating over entire list
+                new_tail.append(line)
+
+
+file_name = 'Black Sabbath - Spiral Architect.txt'
+
+
+song_title = file_name.split(' - ')[1].split('.txt')[0]
+artist = file_name.split(' - ')[0]
+
+cleaned_file_str = artist + ' - ' + song_title + ' - cleaned.txt'
+
+with open(file_name, 'r') as original_file, open(cleaned_file_str, 'w', newline='') as new_file:
+    lyric_list = list(csv.reader(original_file))
+    lyric_obj = LyricsFromText(lyric_list)
+
+    replacement_headers = fix_header()
+    replacement_tail = fix_tail()
+
+    replacement_lyrics = replacement_headers + lyric_obj.middle + replacement_tail
+
+    lyric_writer = csv.writer(new_file)
+
+    for row in replacement_lyrics:
+        lyric_writer.writerow(row)
+
